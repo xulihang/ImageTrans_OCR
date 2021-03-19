84a53d8af50c8c2b79fdabf4ad198cb643c34137
xulihang <xulihanghai@163.com>
2021-03-19 17:33:23 +0800

    directly use keras_ocr's detector instead of pipline

--- craft_detector.py
+++ craft_detector.py
@@ -4,23 +4,21 @@ from detector import Detector
 class CRAFTDetector(Detector):
 
     def __init__(self):
-        self.pipeline = keras_ocr.pipeline.Pipeline()
+        self.detector = keras_ocr.detection.Detector()
         
     def detect(self, image):
         
         images = [keras_ocr.tools.read(image)]
-        prediction_groups = self.pipeline.recognize(images,do_recognition=False)
-        prediction=prediction_groups[0]
+        box_groups = self.detector.detect(images=images)
+        prediction=box_groups[0]
         text_lines=[]
-        print(prediction)
-
         for result in prediction:
             line={}
             index=0
-            for coord in result[1]:
+            for coord in result:
                 line["x"+str(index)]=int(coord[0])
                 line["y"+str(index)]=int(coord[1])
                 index=index+1
-            line["text"]=result[0]
+            line["text"]=""
             text_lines.append(line)
         return text_lines
